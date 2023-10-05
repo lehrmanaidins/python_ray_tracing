9acffd35b6f39640a768a1e14378ae2461bbc5d7
lehrmanaidins <142857908+lehrmanaidins@users.noreply.github.com>
2023-10-05 13:32:04 -0400

    Update Vector3.py

--- Vector3.py
+++ Vector3.py
@@ -11,9 +11,11 @@ class Vector3:
     """ Vector3 Class
     
     Args:
-        _x, _y, _z (floats): _x, _y, and _z values of the Vector3 (Each entered as seperate argument)
+        _x, _y, _z (floats): _x, _y, and _z values of the Vector3
+        (Each entered as seperate argument)
             or
-        vector (Vector3): Creates second Vector3 with same values as argument Vector3 (Only need 1 argument)
+        vector (Vector3): Creates second Vector3 with same values as argument Vector3
+        (Only need 1 argument)
 
     Returns:
         
@@ -35,10 +37,10 @@ class Vector3:
                 pass
         else:
             raise TypeError
-    
+
     def __str__(self) -> str:
         return f'[{self._x}, {self._y}, {self._z}]'
-        
+
     def __add__(self, value: Union[int, float, Vector3]) -> Vector3:
         if isinstance(value, Union[int, float]):
             _x: float = self._x + value
@@ -52,7 +54,7 @@ class Vector3:
             return Vector3(_x, _y, _z)
         else:
             raise TypeError
-        
+
     def __iadd__(self, value: Union[int, float, Vector3]) -> Vector3:
         if isinstance(value, Union[int, float]):
             self._x += value
@@ -66,7 +68,7 @@ class Vector3:
             return self
         else:
             raise TypeError
-        
+
     def __sub__(self, value: Union[int, float, Vector3]) -> Vector3:
         if isinstance(value, Union[int, float]):
             _x: float = self._x - value
@@ -80,7 +82,7 @@ class Vector3:
             return Vector3(_x, _y, _z)
         else:
             raise TypeError
-        
+
     def __isub__(self, value: Union[int, float, Vector3]) -> Vector3:
         if isinstance(value, Union[int, float]):
             self._x -= value
@@ -94,7 +96,7 @@ class Vector3:
             return self
         else:
             raise TypeError
-        
+
     def __mul__(self, value: Union[int, float, Vector3]) -> Vector3:
         if isinstance(value, Union[int, float]):
             _x: float = self._x * value
@@ -108,7 +110,7 @@ class Vector3:
             return Vector3(_x, _y, _z)
         else:
             raise TypeError
-        
+
     def __imul__(self, value: Union[int, float, Vector3]) -> Vector3:
         if isinstance(value, Union[int, float]):
             self._x *= value
@@ -122,7 +124,7 @@ class Vector3:
             return self
         else:
             raise TypeError
-        
+
     def __truediv__(self, value: Union[int, float, Vector3]) -> Vector3:
         if isinstance(value, Union[int, float]):
             try:
@@ -142,7 +144,7 @@ class Vector3:
                 return Vector3(0, 0, 0)
         else:
             raise TypeError
-        
+
     def __itruediv__(self, value: Union[int, float, Vector3]) -> Vector3:
         if isinstance(value, Union[int, float]):
             try:
@@ -162,7 +164,7 @@ class Vector3:
                 return Vector3(0, 0, 0)
         else:
             raise TypeError
-        
+
     def __floordiv__(self, value: Union[int, float, Vector3]) -> Vector3:
         if isinstance(value, Union[int, float]):
             try:
@@ -182,7 +184,7 @@ class Vector3:
                 return Vector3(0, 0, 0)
         else:
             raise TypeError
-        
+
     def __ifloordiv__(self, value: Union[int, float, Vector3]) -> Vector3:
         if isinstance(value, Union[int, float]):
             try:
@@ -202,31 +204,44 @@ class Vector3:
                 return Vector3(0, 0, 0)
         else:
             raise TypeError
-    
+
     def length_squared(self) -> float:
+        """ Calculates sq(length) of Vector3
+        """
         return (self._x ** 2) + (self._y ** 2) + (self._z ** 2)
-    
+
     def length(self) -> float:
+        """ Retruns sqrt(length_squared)
+        """
         return math.sqrt(self.length_squared())
-    
-"""
-    Point Class is used for readability (It is a Vector3, just different name)
-"""
+
+
 class Point3(Vector3):
+    """ Point Class is used for readability (It is a Vector3, just different name)
+    """
     def __str__(self) -> str:
         return f'({self._x}, {self._y}, {self._z})'
 
-    
-"""
-    Vector Utility Functions
-"""
+
 def dot(a: Vector3, b: Vector3) -> float:
+    """ Dot product of two Vector3's
+    """
+    if not (isinstance(a, Vector3) or isinstance(b, Vector3)):
+        raise TypeError
     return (a._x * b._x) + (a._y * b._y) + (a._z * b._z)
 
 def cross(a: Vector3, b: Vector3) -> Vector3:
+    """ Cross product of two Vector3's
+    """
+    if not (isinstance(a, Vector3) or isinstance(b, Vector3)):
+        raise TypeError
     return Vector3(a._y * b._z - a._z * b._y,
                    a._z * b._x - a._x * b._z,
                    a._x * b._y - a._y * b._x)
 
 def unit_vector(a: Vector3) -> Vector3:
+    """ Returns Vector3 with same direction as argument Vector3, but with magnitude of 1
+    """
+    if not isinstance(a, Vector3):
+        raise TypeError
     return a / a.length()
