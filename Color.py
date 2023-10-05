d5aa1f249297d37bfb703ad670ac7b4b63b356f0
lehrmanaidins <142857908+lehrmanaidins@users.noreply.github.com>
2023-10-05 10:37:46 -0400

    Update Color.py

--- Color.py
+++ Color.py
@@ -3,20 +3,29 @@
     @author Lehrman, Aidin
 """
 
-from Vector3 import *
+from math import floor
+import Vector3
 
-class Color(Vector3):
+class Color(Vector3.Vector3):
+    """ Color represented by Vector3 with '_r', '_g', and '_b' values from [0, 1]
+    """
     def __str__(self) -> str:
-        return f'{{{self.x}, {self.y}, {self.z}}}'
+        return f'{{{self._x}, {self._y}, {self._z}}}'
 
     def to_rgb(self) -> list:
-        r: int = min(max(math.floor(self.x * 255), 0), 255) # [0, 255]
-        g: int = min(max(math.floor(self.y * 255), 0), 255) # [0, 255]
-        b: int = min(max(math.floor(self.z * 255), 0), 255) # [0, 255]
-        return [r, g, b]
+        """ Returns Color formated as list of length 3 with 
+            '_x' ([0]), '_y' ([1]), and '_z' ([2]) values scaled to [0, 255]
+        """
+        _r: int = min(max(floor(self._x * 255), 0), 255) # [0, 255]
+        _g: int = min(max(floor(self._y * 255), 0), 255) # [0, 255]
+        _b: int = min(max(floor(self._z * 255), 0), 255) # [0, 255]
+        return [_r, _g, _b]
 
     def to_list(self) -> list:
-        r: float = min(max(self.x, 0), 1) # [0, 1]
-        g: float = min(max(self.y, 0), 1) # [0, 1]
-        b: float = min(max(self.z, 0), 1) # [0, 1]
-        return [r, g, b]
+        """ Returns Color formated as list of length 3 with 
+            '_x' ([0]), '_y' ([1]), and '_z' ([2]) values scaled to [0, 1]
+        """
+        _r: float = min(max(self._x, 0), 1) # [0, 1]
+        _g: float = min(max(self._y, 0), 1) # [0, 1]
+        _b: float = min(max(self._z, 0), 1) # [0, 1]
+        return [_r, _g, _b]
