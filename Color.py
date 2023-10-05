fa28203f0f78b31a028df011e1ce773bfcaa83cc
lehrmanaidins <142857908+lehrmanaidins@users.noreply.github.com>
2023-10-05 08:02:18 -0400

    Update Color.py

--- Color.py
+++ Color.py
@@ -15,3 +15,8 @@ class Color(Vector3):
         b: int = min(max(math.floor(self.z * 255.999), 0), 255) # [0, 255]
         return [r, g, b]
 
+    def to_list(self) -> list:
+        r: float = min(max(self.x, 0), 1) # [0, 1]
+        g: float = min(max(self.y, 0), 1) # [0, 1]
+        b: float = min(max(self.z, 0), 1) # [0, 1]
+        return [r, g, b]
