e8ce8a50accf195797c097437866c61e0ae26d72
lehrmanaidins <142857908+lehrmanaidins@users.noreply.github.com>
2023-10-05 12:30:35 +0000

    "Commit"

--- main.py
+++ main.py
@@ -9,20 +9,22 @@ from Color import *
 from Vector3 import *
 
 # Image
-image_height = 256 # Height of image
-image_width = 256 # Width of image
-image = np.zeros((image_height, image_width, 3)) # Creates 2D array of [r, g, b] colors with width 'image_width' and height 'image_height'
+IMAGE_HEIGHT: int = 256 # Height of image
+IMAGE_WIDTH: int = 256 # Width of image
 
-for j in range(image_height): # For each row
+# Creates 2D array of [r, g, b] colors with width 'image_width' and height 'image_height'
+image = np.zeros((IMAGE_HEIGHT, IMAGE_WIDTH, 3))
+
+for j in range(IMAGE_HEIGHT): # For each row
     
-    scanlines_remaining = image_height - j # Calculates the amount of rows remaining
+    scanlines_remaining = IMAGE_HEIGHT - j # Calculates the amount of rows remaining
     
     # Prints progress bar and percent
-    print(f"\rScanlines Remaining:\t{' ' * ( 3 - len(str(scanlines_remaining)))}{scanlines_remaining}\t|{'#' * (j / 20)}{' ' * (scanlines_remaining / 20)}|\t{j / image_height * 100:.2f}%", end='')
+    print(f"\rScanlines Remaining:\t{' ' * ( 3 - len(str(scanlines_remaining)))}{scanlines_remaining}\t|{'#' * math.floor(j / 20)}{' ' * math.floor(scanlines_remaining / 20)}|\t{j / IMAGE_HEIGHT * 100:.2f}%", end='')
     
-    for i in range(image_width): # For each pixel in each row
+    for i in range(IMAGE_WIDTH): # For each pixel in each row
         
-        pixel_color = Color(i / (image_width - 1), j / (image_height - 1), 0) # Gets pixel colors based on pixel's (x, y) values
+        pixel_color = Color(i / (IMAGE_WIDTH - 1), j / (IMAGE_HEIGHT - 1), 0) # Gets pixel colors based on pixel's (x, y) values
         image[i, j] = np.clip(pixel_color.to_list(), 0, 1) # Sets pixel color to 'pixel_color'
 
 plt.imsave('image.png', image) # Saves image
