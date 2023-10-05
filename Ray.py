b8d7241a9c16c601deea7b329903083998e1f699
lehrmanaidins <lehrmanaidins+github.com@gmail.com>
2023-10-05 09:32:24 -0400

    "Ray class added"

---
+++ Ray.py
@@ -0,0 +1,38 @@
+"""
+    Ray Class
+    @author Lehrman, Aidin
+
+"""
+
+from Vector3 import *
+
+class Ray:
+    """ Ray is made up of two parts; a point in 3D space, and a vector in a direction
+    
+    Args:
+        origin (Point3): Origin point of the ray
+        direction (Vector3): Direction the ray is pointing towards
+
+    Returns:
+        
+    """
+    def __init__(self, origin: Point3, direction: Vector3) -> None:
+        if not (isinstance(origin, Vector3) and isinstance(direction, Vector3)):
+            raise TypeError
+        self.origin = origin
+        self.direction = direction
+
+    def __str__(self) -> str:
+        return f'{str(self.origin)} -> {str(self.direction)}'
+    
+    def at(self, t: float) -> Point3:
+        """ Returns point that is on ray 't' distance from ray's origin
+        :t: Distance from origin
+        :t type: float or int
+        :returns: Point3 on ray 't' distnace from origin
+        :rtype: Point3
+        """
+        if not (isinstance(t, float) or isinstance(t, int)):
+            raise TypeError
+        return self.origin + (self.direction * t)
+    
