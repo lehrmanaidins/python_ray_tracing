a71b7ac40e2b0e446337559e06fd3e79b555142b
lehrmanaidins <lehrmanaidin+github.com@gmail.com>
2023-10-05 22:18:16 -0400

    "Removed second of two pastes of Ray class"

--- environment_variables.py
+++ environment_variables.py
@@ -278,34 +278,3 @@ class Ray3:
         if not (isinstance(distance, float) or isinstance(distance, int)):
             raise TypeError
         return self.origin + (self.direction * distance)
-    
-class Ray3:
-    """ Ray3 is made up of two parts; a point in 3D space, and a vector in a direction
-    
-    Args:
-        origin (Point3): Origin point of the ray
-        direction (Vector3): Direction the ray is pointing towards
-
-    Returns:
-        3D Ray from Point3 'origin' in direction 'direction' with infinite magnitude
-    
-    """
-    def __init__(self, origin: Point3, direction: Vector3) -> None:
-        if not (isinstance(origin, Vector3) and isinstance(direction, Vector3)):
-            raise TypeError
-        self.origin = origin
-        self.direction = direction
-
-    def __str__(self) -> str:
-        return f'{str(self.origin)} -> {str(self.direction)}'
-    
-    def at(self, t: Union[int, float]) -> Point3:
-        """ Returns point that is on ray 't' distance from ray's origin
-        :t: Distance from origin
-        :t type: float or int
-        :returns: Point3 on ray 't' distnace from origin
-        :rtype: Point3
-        """
-        if not (isinstance(t, float) or isinstance(t, int)):
-            raise TypeError
-        return self.origin + (self.direction * t)
